ab89a561d8d59be17ae23798e499a0d3c858f664
The-Dark-Knight <solomonnderitu99@gmail.com>
2025-06-09 11:09:34 +0300

    Just rewrote it with minor changes

--- downloader.py
+++ downloader.py
@@ -16,25 +16,23 @@ def downloader():
     question = input("Do you have more than one video you`d like to download? (Y/n): ").lower()
     if question == 'y':
         while True:
-            line = input("Enter Video URL or type(q) to start downloading: ")
-            if line.lower() == 'q':
-                break
-            else:
-                URLS.append(line)
+             line = input("Enter Video URL or type(q) to start downloading: ")
+             if line.lower() == 'q':
+                 break
+             else:
+                 URLS.append(line)
     else:
         URLS.append(input('Enter Video URL: '))
 
-    question = input("Do you want audio output only (Y/n): ").lower()
+    question = input("Do you want audio output only Y/n: ").lower()
 
     if question == 'y':
         ydl_opts['format'] = 'bestaudio[ext=m4a]/best,'
     else:
         ydl_opts['format'] = 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best,'
 
-    
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download(URLS)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     downloader()
